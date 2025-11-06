7050292d18dd013818b2c7d4dd7c28c77df6b7e0
mouni11998 <mounikavanipenta95@gmail.com>
2025-11-06 13:19:13 +0530

    Initial commit - My portfolio project

--- mounika-portfolio-react-main/src/App.jsx
+++ mounika-portfolio-react-main/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import Portfolio from "./poojitha_portfolio_react.jsx"; // ensure the filename matches
+import Portfolio from "./mounika_portfolio_react.jsx"; // ensure the filename matches
 
 export default function App() {
   return <Portfolio />;
